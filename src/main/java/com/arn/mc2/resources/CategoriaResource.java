a99169186028582f8cfe797b439306b17a0d7317
whitefang-code <lucianoarn5@gmail.com>
2022-10-26 10:07:11 -0300

    Testando a primeira classe de dominio - categoria

---
+++ src/main/java/com/arn/mc2/resources/CategoriaResource.java
@@ -0,0 +1,29 @@
+package com.arn.mc2.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.arn.mc2.domain.Categoria;
+
+@RestController
+@RequestMapping("/categorias")
+
+public class CategoriaResource {
+	@RequestMapping(method=RequestMethod.GET)
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+				
+		return lista;
+		
+	}
+
+}
