222525fe27215240af871b47a66deecc27f4b7b5
whitefang-code <lucianoarn5@gmail.com>
2022-10-26 09:26:38 -0300

    Projeto Criado

---
+++ src/main/java/com/arn/mc2/Cursomc2Application.java
@@ -0,0 +1,13 @@
+package com.arn.mc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc2Application.class, args);
+	}
+
+}
