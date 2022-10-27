799001a7d141901987fb2303ec0c982e54ae92d9
whitefang-code <lucianoarn5@gmail.com>
2022-10-27 10:11:43 -0300

    Criando repository e service para categoria

---
+++ src/main/java/com/arn/mc2/services/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.arn.mc2.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arn.mc2.domain.Categoria;
+import com.arn.mc2.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	@Autowired	
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional <Categoria> obj = repo.findById(id);
+		return obj.orElse(null) ;
+	}
+}
