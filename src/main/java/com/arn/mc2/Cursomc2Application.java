4708683112548d404e024b12b305a15514d64fc4
whitefang-code <lucianoarn5@gmail.com>
2022-11-05 20:50:05 -0300

    Estado e cidade

--- src/main/java/com/arn/mc2/Cursomc2Application.java
+++ src/main/java/com/arn/mc2/Cursomc2Application.java
@@ -9,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.arn.mc2.domain.Categoria;
+import com.arn.mc2.domain.Cidade;
+import com.arn.mc2.domain.Estado;
 import com.arn.mc2.domain.Produto;
 import com.arn.mc2.repositories.CategoriaRepository;
+import com.arn.mc2.repositories.CidadeRepository;
+import com.arn.mc2.repositories.EstadoRepository;
 import com.arn.mc2.repositories.ProdutoRepository;
 
 
@@ -22,6 +26,14 @@ public class Cursomc2Application implements CommandLineRunner {
 	@Autowired
 	
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc2Application.class, args);
@@ -44,6 +56,21 @@ public class Cursomc2Application implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null,"São paulo");
+		
+		Cidade c1 = new Cidade(null,"Uberlândia", est1);
+		Cidade c2 = new Cidade(null,"São Paulo", est2);
+		Cidade c3 = new Cidade(null,"Campinas",est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2,c3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1,est2));
+		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
+		
+		
 		
 	}
 	
---
+++ src/main/java/com/arn/mc2/repositories/CidadeRepository.java
@@ -0,0 +1,13 @@
+package com.arn.mc2.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.arn.mc2.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+	
+
+}
