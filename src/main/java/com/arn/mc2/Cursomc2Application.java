cc4cbb5d6148d89958e151b777fb9e4431b7a988
whitefang-code <lucianoarn5@gmail.com>
2022-11-04 23:52:12 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/arn/mc2/Cursomc2Application.java
+++ src/main/java/com/arn/mc2/Cursomc2Application.java
@@ -1,4 +1,4 @@
-package com.arn.mc2;
+	package com.arn.mc2;
 
 
 import java.util.Arrays;
@@ -9,13 +9,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.arn.mc2.domain.Categoria;
+import com.arn.mc2.domain.Produto;
 import com.arn.mc2.repositories.CategoriaRepository;
+import com.arn.mc2.repositories.ProdutoRepository;
 
 
 @SpringBootApplication
 public class Cursomc2Application implements CommandLineRunner {
 	@Autowired
 	private CategoriaRepository categoriaRepository; 
+	
+	@Autowired
+	
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Cursomc2Application.class, args);
@@ -25,7 +31,19 @@ public class Cursomc2Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null,"Informática");
 		Categoria cat2 = new Categoria(null,"Escritorio");
+		
+		Produto p1 = new Produto(null,"Computador",2000.00);
+		Produto p2 = new Produto(null,"Impressora",800.00);
+		Produto p3 = new Produto(null,"Mouse",80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 	}
 	
