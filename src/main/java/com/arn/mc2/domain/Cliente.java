582a01f8c5b4e3dfee9b9f512429bf24af7a559c
whitefang-code <lucianoarn5@gmail.com>
2022-11-07 16:10:00 -0300

    Somento Jsonignore

--- src/main/java/com/arn/mc2/domain/Cliente.java
+++ src/main/java/com/arn/mc2/domain/Cliente.java
@@ -16,8 +16,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.arn.mc2.domain.enums.TipoCliente;
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Cliente implements Serializable{
@@ -30,13 +29,14 @@ public class Cliente implements Serializable{
 	private String cpfOuCnpj;
 	private Integer tipo;
 	
-	@JsonManagedReference
+	
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
-	@JsonBackReference
+	
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	public Cliente(){
--- src/main/java/com/arn/mc2/domain/Pagamento.java
+++ src/main/java/com/arn/mc2/domain/Pagamento.java
@@ -12,7 +12,7 @@ import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
 
 import com.arn.mc2.domain.enums.EstadoPagamento;
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Inheritance(strategy=InheritanceType.JOINED)
@@ -22,7 +22,7 @@ public abstract class Pagamento implements Serializable{
 	private Integer id;
 	private Integer estado;
 	
-	@JsonBackReference
+	@JsonIgnore	
 	@OneToOne
 	@JoinColumn(name="pedido_id")
 	@MapsId
